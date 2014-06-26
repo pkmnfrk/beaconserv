2cc34b144bc28a90e1e130ffe3c53a5b1ece3546
Mike Caron <caron.mike@gmail.com>
2014-06-26 11:54:00 -0400

    Add a spinner when not connected

--- NodeBeacon/static/util/scripts/fullscreen.js
+++ NodeBeacon/static/util/scripts/fullscreen.js
@@ -1,20 +1,7 @@
-var myname = window.location.search.substring(1);
 
-
-
-
-var loc = window.location, new_uri;
-if (loc.protocol === "https:") {
-    new_uri = "wss:";
-} else {
-    new_uri = "ws:";
-}
-new_uri += "//" + loc.host;
-new_uri += "/display";
-
-var websocket = new WebSocket(new_uri);
-
-websocket.onmessage = function(msg) {
+function onmessage(msg) {
+    $("#loader").hide();
+    
     msg = JSON.parse(msg.data);
     
     if(msg.hello) {
@@ -30,7 +17,8 @@ websocket.onmessage = function(msg) {
         }
         
     }
-};
+}
+
 
 function toggleFullScreen() {
   if (!document.fullscreenElement &&    // alternative standard method
@@ -56,5 +44,46 @@ function toggleFullScreen() {
     }
   }
 }
+
+function tryConnect() {
+    if(errorRetry) {
+        clearTimeout(errorRetry);
+        errorRetry = null;
+    }
+    
+    
+    
+    websocket = new WebSocket(new_uri);
+
+    websocket.onmessage = onmessage;
+    
+    var onfud = function() {
+        $("#loader").show();
+        
+        errorRetry = setTimeout(tryConnect, 1000);
+    };
+    
+    websocket.onerror = onfud;
+    websocket.onclose = onfud;
+    
+}
+
+var errorRetry = null;
+
+
+var myname = window.location.search.substring(1);
+
+var loc = window.location, new_uri;
+if (loc.protocol === "https:") {
+    new_uri = "wss:";
+} else {
+    new_uri = "ws:";
+}
+new_uri += "//" + loc.host;
+new_uri += "/display";
+
+var websocket = null;
+
+tryConnect();
 
 $(".fsbutton").click(toggleFullScreen);
